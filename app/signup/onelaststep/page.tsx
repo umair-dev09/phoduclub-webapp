18a198e8255d61749c1340543a2a3991420f2f8c
umair-dev09 <shaikhumair5002@gmail.com>
2024-08-26 18:47:30 +0530

    laststep page changes

--- app/signup/onelaststep/page.tsx
+++ app/signup/onelaststep/page.tsx
@@ -1,31 +1,27 @@
 "use client";
-
-import React, { useState } from "react";
-import Select from "react-select";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect, useRef } from "react";
 import './lastStep.css';
+import { supabase } from "@/utils/supabase/client";
+import Image from 'next/image';
+import React from 'react';
+import Dropdown from '../../../components/Dropdown';
+import styles from '../styles/Dropdown.module.css';
 
-const targetExamOptions = [
-  { value: 'BITSAT', label: 'BITSAT' },
-  { value: 'JEE', label: 'JEE' },
-  { value: 'SRMJEEE', label: 'SRMJEEE' },
-  { value: 'COMEDK', label: 'COMEDK' },
-  { value: 'KCET', label: 'KCET' },
-  { value: 'VITEEE', label: 'VITEEE' },
-  { value: 'MET', label: 'MET' }
-];
-
-export default function Home() {
-  const [selectedExams, setSelectedExams] = useState([]);
-
-  const handleExamChange = (selectedOptions) => {
-    setSelectedExams(selectedOptions);
-  };
+export default function OneLastStep() {
+  
+  
 
   return (
     <div className="container">
       <div className="verify">
         <div className="logo">
-          <p>phodu.club</p>
+        <Image
+                src="/images/phoduclublogo.png" // Path to your image file
+              alt="Description of image"
+              width={150} // Desired width
+               height={25} // Desired height
+                 />
         </div>
         <div className="return">
           <a href="">&larr; Back</a>
@@ -33,41 +29,25 @@ export default function Home() {
         <div className="container-2">
           <div className="heading">
             <p className="head">One Last Step</p>
-          </div> 
-          <div className="targetyear"><p>Target Year</p></div>
-          <div className="targetyearselection">
-            <select name="year" id="targetyear" className="yearselector" aria-multiline>
-              <option value="2024">2024</option>
-              <option value="2025">2025</option>
-              <option value="2026">2026</option>
-            </select>
           </div>
-          <div className="targetexam"><p>Target Exam</p></div>
-          <div className="targetexamselection">
-            <Select
-              isMulti
-              name="exams"
-              options={targetExamOptions}
-              className="examselector"
-              classNamePrefix="select"
-              value={selectedExams}
-              onChange={handleExamChange}
-            />
+          <div className="dropDowns">
+          <Dropdown />
           </div>
+
           <div className="buttons">
-            <button className="button">Sign Up</button>
+          <button className="button">Sign Up</button>
           </div>
         </div>
       </div>
       <div className="quote-section">
-        <div className="inv_comma"></div>
-        <div className="quote">
-          <p>Education is the foundation upon which we build our future.</p>
-        </div>
-        <div className="teller">
-          <p>— Christine Gregoire</p>
-        </div>
+        <Image
+          src="/images/test1.png" // Path to your image file
+          alt="Description of image"
+          width={10000} // Desired width
+          height={10000} // Desired height
+        />
       </div>
-    </div>
-  );
+    </div>
+  );
 }
+
