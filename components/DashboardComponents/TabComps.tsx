4a83093bdf946a7cc6346b92636d10dc7014da1f
VJKarthikeya <jathinkarthikeyavulli@gmail.com>
2024-08-31 22:52:04 +0530

    changes made in dashboard

--- components/DashboardComponents/TabComps.tsx
+++ components/DashboardComponents/TabComps.tsx
@@ -15,18 +15,19 @@ function TabComps() {
     };
 
     // Function to handle tab clicks and set the active tab
-    const handleTabClick = (tabName, path) => {
-        setActiveTab(tabName);  // Update active tab state
-        router.push(path);      // Navigate to the desired path
+    const handleTabClick = (tabName: string , path: string) => {
+        setActiveTab(tabName);
+        router.push(path);
     };
+    
 
     return (
         <div className={`${styles.tabComps} ${isCollapsed ? styles.collapsed : ''}`}>
             <div className={styles.collapseButton}>
-                <button className={styles.collapseIcon} onClick={handleCollapseClick}>{isCollapsed ? '>' : '<'}</button>
+                <button className={styles.collapseIcon} onClick={handleCollapseClick}>
+                    {isCollapsed ? <Image  className={styles.collapseIconRight} src="/icons/collapseIconRight.png" width={10} height={10}/> : <Image  className={styles.collapseIconLeft} src="/icons/Vector (3).png" width={10} height={10}/>}
+                </button>
             </div>
-            {!isCollapsed && (
-                <>
                     <div className={styles.logo}>
                         <div className={styles.phoduClubSymbol}>
                             <p className={styles.theSymbol}>P</p>
@@ -36,47 +37,45 @@ function TabComps() {
                         </div>
                     </div>
                     <div className={styles.divider}>
-                        <hr />
+                        <hr className={styles.actualDivider}/>
                     </div>
-                    <div className={styles.tabs}>
-                        <button 
-                            onClick={() => handleTabClick('dashboard', '/dashboard')}
-                            className={`${styles.DashboardButton} ${activeTab === 'dashboard' ? styles.active : ''}`}
-                        >
-                            <Image className={styles.dashboardIcon} src="/icons/dashboard-speed-02.png" width={20} height={20} alt="Dashboard Icon" />
-                            <p className={styles.text}>Dashboard</p>
-                        </button>
-                        <button 
-                            onClick={() => handleTabClick('learn', '/learn')}
-                            className={`${styles.LearnButton} ${activeTab === 'learn' ? styles.active : ''}`}
-                        >
-                            <Image className={styles.learnIcon} src="/icons/book-open-01.png" width={20} height={20} alt="Learn Icon"/>           
-                            <p className={styles.text}>Learn</p>
-                        </button>
-                        <button 
-                            onClick={() => handleTabClick('community', '/community')}
-                            className={`${styles.CommunitiesButton} ${activeTab === 'community' ? styles.active : ''}`}
-                        >
-                            <Image className={styles.communitiesIcon} src="/icons/community-icon.png" width={20} height={20} alt="Communities Icon"/>
-                            <p className={styles.text}>Communities</p>
-                        </button>
-                        <button 
-                            onClick={() => handleTabClick('analytics', '/analytics')}
-                            className={`${styles.AnalyticsButton} ${activeTab === 'analytics' ? styles.active : ''}`}
-                        >
-                            <Image className={styles.analyticsIcon} src="/icons/analytics-icon.png" width={20} height={20} alt="Analytics Icon"/>
-                            <p className={styles.text}>Analytics</p>
-                        </button>
-                        <button 
-                            onClick={() => handleTabClick('settings', '/settings')}
-                            className={`${styles.SettingsButton} ${activeTab === 'settings' ? styles.active : ''}`}
-                        >
-                            <Image className={styles.settingsIcon} src="/icons/settings-03.png" width={20} height={20} alt="Settings Icon"/>
-                            <p className={styles.text}>Settings</p>
-                        </button>
-                    </div>
-                </>
-            )}
+            <div className={styles.tabs}>
+                <button 
+                    onClick={() => handleTabClick('dashboard', '/dashboard')}
+                    className={`${styles.DashboardButton} ${activeTab === 'dashboard' ? styles.active : ''}`}
+                >
+                    <Image className={styles.dashboardIcon} src="/icons/dashboard-speed-02.png" width={20} height={20} alt="Dashboard Icon" />
+                    {!isCollapsed && <p className={styles.text}>Dashboard</p>}
+                </button>
+                <button 
+                    onClick={() => handleTabClick('learn', '/learn')}
+                    className={`${styles.LearnButton} ${activeTab === 'learn' ? styles.active : ''}`}
+                >
+                    <Image className={styles.learnIcon} src="/icons/book-open-01.png" width={20} height={20} alt="Learn Icon"/>           
+                    {!isCollapsed && <p className={styles.text}>Learn</p>}
+                </button>
+                <button 
+                    onClick={() => handleTabClick('community', '/community')}
+                    className={`${styles.CommunitiesButton} ${activeTab === 'community' ? styles.active : ''}`}
+                >
+                    <Image className={styles.communitiesIcon} src="/icons/community-icon.png" width={20} height={20} alt="Communities Icon"/>
+                    {!isCollapsed && <p className={styles.text}>Communities</p>}
+                </button>
+                <button 
+                    onClick={() => handleTabClick('analytics', '/analytics')}
+                    className={`${styles.AnalyticsButton} ${activeTab === 'analytics' ? styles.active : ''}`}
+                >
+                    <Image className={styles.analyticsIcon} src="/icons/analytics-icon.png" width={20} height={20} alt="Analytics Icon"/>
+                    {!isCollapsed && <p className={styles.text}>Analytics</p>}
+                </button>
+                <button 
+                    onClick={() => handleTabClick('settings', '/settings')}
+                    className={`${styles.SettingsButton} ${activeTab === 'settings' ? styles.active : ''}`}
+                >
+                    <Image className={styles.settingsIcon} src="/icons/settings-03.png" width={20} height={20} alt="Settings Icon"/>
+                    {!isCollapsed && <p className={styles.text}>Settings</p>}
+                </button>
+            </div>
         </div>
     );
 }
