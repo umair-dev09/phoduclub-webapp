4d1d37f3f2cb9f496b11bc57b4eedb438ac5f822
VJKarthikeya <jathinkarthikeyavulli@gmail.com>
2024-09-07 18:02:03 +0530

    home page changes done
    Co-authored-by: Jabir Ali <jabir12-12@users.noreply.github.com>

--- components/DashboardComponents/HomeComponents/Course/Course.tsx
+++ components/DashboardComponents/HomeComponents/Course/Course.tsx
@@ -34,12 +34,12 @@ function Course(){
                             <p>BITSET Full Course</p>
                         </div>
                         <div className={styles.courseLength}>
-                            <p>3 Lessons</p>&#x2022;<p>3hr 14m</p>
+                            <p className={styles.courseLength1}>3 Lessons </p>&#x2022;<p className={styles.courseLength2}>3hr 14m</p>
                         </div>
                     </div>
                     <div className={styles.coursePrice}>
                         <div className={styles.courseAmount}>
-                            <h3>&#8377; 2400</h3>
+                            <h4>&#8377; 2400</h4>
                         </div>
                         <div className={styles.courseBuy}>
                             <button className={styles.buyButton}>Buy Now</button>
@@ -81,7 +81,7 @@ function Course(){
                     </div>
                     <div className={styles.coursePrice}>
                         <div className={styles.courseAmount}>
-                            <h3>&#8377; 2400</h3>
+                            <h4>&#8377; 2400</h4>
                         </div>
                         <div className={styles.courseBuy}>
                             <button className={styles.buyButton}>Buy Now</button>
---
+++ components/DashboardComponents/HomeComponents/TestSeries/testSeriesComp/testSeriesComp.tsx
@@ -0,0 +1,3 @@
+'use client';
+
+import styles from './testSeriesComp.module.css';
