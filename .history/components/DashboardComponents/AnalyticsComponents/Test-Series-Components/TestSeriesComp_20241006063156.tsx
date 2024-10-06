27e8af28e0771995e078503b6fe6b6a32ef29507
jabir12-12 <jabirali7975@gmail.com>
2024-10-06 12:02:08 +0530

    changes by jabir

---
+++ .history/components/DashboardComponents/AnalyticsComponents/Test-Series-Components/TestSeriesComp_20241006063156.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+function TestSeriesComp() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const [activeTab, setActiveTab] = useState<string>('');
+
+    const handleTabClick = (tabName: string, path: string) => {
+        setActiveTab(tabName);
+        router.push(path);
+    };
+    useEffect(() => {
+        if (pathname) {
+            const currentPath = pathname.split('/')[3]
+            if (currentPath === 'PhoduJeeMainsTestSeries') {
+                setActiveTab('PhoduJeeMainsTestSeries')
+            }
+
+        }
+    }, [pathname]);
+
+    return (
+
+        <button className="hover:bg-[#F9FAFB] "
+            onClick={() => handleTabClick("PhoduJeeMainsTestSeries", "/analytics/test-series/PhoduJeeMainsTestSeries")}>
+
+            <tr className="flex flex-1 py-3 border-t border-lightGrey text-[#1D2939]">
+                <td className="flex flex-col px-8 w-[30%] text-left">
+
+                </td>
+                <td className="flex justify-center items-center w-[15%]">932</td>
+                <td className="flex justify-center items-center w-[15%]">143/150</td>
+                <td className="flex justify-center items-center w-[15%]">80%</td>
+                <td className="flex justify-center items-center w-[15%]">45h 30m</td>
+                <td className="flex justify-center items-center w-[15%]"><p className="text-end w-16">2h</p></td>
+            </tr>
+
+        </button>
+    );
+}
+
+export default TestSeriesComp;
