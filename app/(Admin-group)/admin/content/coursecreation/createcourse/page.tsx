7da407cf0f6f6bbafe70b3253b67f0602914afe9
umair-dev09 <shaikhumair5002@gmail.com>
2024-11-05 22:02:40 +0530

    new changes

--- app/(Admin-group)/admin/content/coursecreation/createcourse/page.tsx
+++ app/(Admin-group)/admin/content/coursecreation/createcourse/page.tsx
@@ -6,13 +6,8 @@ import ReactQuill from 'react-quill'; // Ensure correct import
 import Quill from 'quill'; // Import Quill to use it for types
 import { Popover, PopoverTrigger, PopoverContent } from '@nextui-org/popover';
 import { useRouter } from "next/navigation";
-interface priceprops {
-    Price: number;
-    Discountprice: number;
 
-
-}
-function createcourse({ Price, Discountprice }: priceprops) {
+function createcourse() {
     // state for ReactQuill
     const [value, setValue] = useState('');
     const quillRef = useRef<ReactQuill | null>(null); // Ref to hold ReactQuill instance
@@ -142,6 +137,7 @@ function createcourse({ Price, Discountprice }: priceprops) {
                     </button>
                     <button className="h-[44px] w-[120px] ml-4 rounded-md items-center flex border border-solid border-[#800EE2] bg-[#9012FF] justify-center shadow-inner-button"
                         onClick={() => handleTabClick('/admin/content/coursecreation/createcourse/courses')}>
+                            
                         <span className="text-[#FFFFFF] font-semibold text-sm">Create</span>
                     </button>
                 </div>
@@ -252,7 +248,7 @@ function createcourse({ Price, Discountprice }: priceprops) {
                     {/* Pricing of the Courses */}
                     <div className="flex flex-row w-full gap-4">
                         <div className="flex flex-col gap-1 w-1/2 flex-grow">
-                            <label htmlFor="discount-price" className="text-[#1D2939] text-sm font-medium">{Price}</label>
+                            <label htmlFor="discount-price" className="text-[#1D2939] text-sm font-medium"></label>
                             <div className="flex flex-row py-2 px-4 w-full gap-2 border border-solid border-[#D0D5DD] rounded-md transition duration-200 ease-in-out focus:border-red-300">
                                 {price && <div className="text-[#1D2939]">₹</div>}
                                 <input
@@ -266,7 +262,7 @@ function createcourse({ Price, Discountprice }: priceprops) {
                             </div>
                         </div>
                         <div className="flex flex-col gap-1 w-1/2 flex-grow">
-                            <label htmlFor="discount-price" className="text-[#1D2939] text-sm font-medium">{Discountprice}</label>
+                            <label htmlFor="discount-price" className="text-[#1D2939] text-sm font-medium"></label>
                             <div className="flex flex-row py-2 px-4 w-full gap-2 border border-solid border-[#D0D5DD] rounded-md transition duration-200 ease-in-out focus:border-red-300">
                                 {discountPrice && <div className="text-[#1D2939]">₹</div>}
                                 <input
