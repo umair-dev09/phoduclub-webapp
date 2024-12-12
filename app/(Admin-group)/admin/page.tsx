b4439640dce34e01c5101ee3493a4d8f826b6bfd
umair-dev09 <shaikhumair5002@gmail.com>
2024-12-12 23:32:18 +0530

    new changes

--- app/(Admin-group)/admin/page.tsx
+++ app/(Admin-group)/admin/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
-    return (
-        <div>
-            jabir ali
-        </div>
-    )
+function Admin() {
+
+ return(
+    <div className='flex flex-col w-full h-full justify-center items-center'>
+        <h3 className='text-xl'>Welcome to phodu.club</h3>
+    </div>
+ );
+
 }
-
-export default page
+export default Admin;
