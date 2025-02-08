192296b02d186d96db621fc2247ab53f45606794
jabir12-12 <jabirali7975@gmail.com>
2025-02-08 21:24:03 +0530

    changes by jabir

--- .history/app/(dashboard-group)/learn/courses/[courseName]/page_20250208204326.tsx
+++ .history/app/(dashboard-group)/learn/courses/[courseName]/page_20250208204326.tsx
@@ -835,11 +835,33 @@ function Course() {
         </>
       ) : (
 
-        <div className="flex flex-col h-full w-full items-center justify-center">
-          <h3>You does not have access to this course</h3>
-          <button onClick={() => router.back()}><p className="underline text-purple">Go Back</p></button>
+        // <div className="flex flex-col h-full w-full items-center justify-center">
+        //   <h3>You does not have access to this course</h3>
+        //   <button onClick={() => router.back()}><p className="underline text-purple">Go Back</p></button>
+        // </div>
+        <div className="h-screen w-full flex items-center justify-center px-4">
+          <div className="flex flex-col items-center justify-center gap-4 max-w-md text-center">
+            <Image
+              src="/icons/not-found-image.svg"
+              width={120}
+              height={120}
+              alt="not-found"
+              className="w-24 h-24 sm:w-32 sm:h-32"
+            />
+            <h1 className="text-[#101828] font-bold text-xl sm:text-2xl">
+              Something went wrong
+            </h1>
+            <span className="font-medium text-sm sm:text-base text-[#667085]">
+              Sorry, we couldn’t find the page you were looking for.
+            </span>
+            <button
+              className="font-semibold text-sm sm:text-base text-[#9012FF] bg-[#F9F5FF] hover:bg-[#E0C9FF] px-4 py-2 rounded-xl transition duration-200"
+              onClick={() => router.push("/dashboard")}
+            >
+              Go back home
+            </button>
+          </div>
         </div>
-
       )}
       <ToastContainer />
     </div>
